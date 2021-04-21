4dab96fdb23fbb589b03a29dcf9e9cf603b7d2e5
nathaniel <njm2015@gmail.com>
2021-04-20 22:46:47 -0500

    asdf

--- binance/ingestion/ada.py
+++ binance/ingestion/ada.py
@@ -21,6 +21,7 @@ while True:
 	res = requests.get('https://api.binance.us/api/v3/ticker/bookTicker', headers=headers, params=params)
 
 	res_json = res.json()
+	print(res_json)
 
 	cursor = conn.cursor()
 	cursor.execute('''
